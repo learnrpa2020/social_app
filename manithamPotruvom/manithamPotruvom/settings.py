05c3985422f7ca9e0d6ff8b24edb4ebc6ad474bd
Maria Monica <learnrpa2020@gmail.com>
2022-05-15 18:35:39 +0530

    changes done

--- manithamPotruvom/manithamPotruvom/settings.py
+++ manithamPotruvom/manithamPotruvom/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_DIRS=(os.path.join(BASE_DIR,'static/'),)
+STATIC_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 
 MEDIA_URL = '/media/'
